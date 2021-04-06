c7e7040ee3ce395601e1e91a07b423221ffd0796
thestreaky <jiachaowu@hotmail.com>
2021-04-05 23:40:22 -0400

    upload

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1> {{title}}</h1>
+        <Button text = "Add task" color = "green"/>
+         
+         
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+export default {
+    name:'Header',
+    props:{
+        title:{
+            type:String,
+            Default: 'hello world',
+        }
+    },
+    components:{
+        Button
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content:space-between;
+        align-items:center;
+        margin-bottom: 20px;
+    }   
+</style>
